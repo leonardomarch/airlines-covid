107c743d042ebbfb8fb5430edf945935845bc120
Leonardo March <Mneyid@Leonardos-iMac.home>
2020-05-01 10:57:19 -0400

    update graph

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ $("body").on("click", "svg rect", function (e) {
 	e.preventDefault();
 	const this_rect = e.target;
 
-	const result = $(this_rect).attr("mydata:name")
-	$(this_rect).removeClass("active");
+	const stock = $(this_rect).attr("mydata:stock");
+	const fare = $(this_rect).attr("mydata:fare");
+	const cases = $(this_rect).attr("mydata:cases");
+
+	$("rect").removeClass("active")
 	$(this_rect).addClass("active");
 
+	$("#fareoutput").text(fare);
+	$("#stockoutput").text(stock);
+	$("#casesoutput").text(cases);
 
-	console.log(result)
+	console.log(airfare + stock)
 
 });
+
+//navigation via buttons//
