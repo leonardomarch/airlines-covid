ccf86adfa5735e45ecdded86991e3d2046f5f9d9
Leonardo March <Mneyid@Leonardos-iMac.home>
2020-05-04 17:08:38 -0400

    details

--- main.js
+++ main.js
@@ -14,7 +14,36 @@ $("body").on("click", "svg rect", function (e) {
 	$("#fareoutput span").text(fare);
 	$("#stockoutput span").text(stock);
 	$("#casesoutput span").text(cases);
+	console.log(stock)
+
+
+	//navigation via buttons//
+
+	//change height of stock prices//
+
+	var newHeight = document.getElementById("stock1");
+	newHeight.style.height = stock + "px";
+
+	var newHeight = document.getElementById("stock2");
+	newHeight.style.height = fare + "px";
+
+	var newHeight = document.getElementById("stock3");
+	newHeight.style.height = cases + "px";
+
+	$("#stock2 span").text(fare);
+	$("#stock1 span").text(stock);
+	$("#stock3 span").text(cases);
+
+	//VIsual cases//
+
+	var person = document.getElementById("person1");
+
+
+	for (let i = 1; i < 10; i++) {
+
+		var outPerson = person.cloneNode(true)
+		$("#casesoutput").append(outPerson);
+	}
+
 
 });
-
-//navigation via buttons//
